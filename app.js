02e4b2144b135fe6e77fec4dec50810d6969f41f
Dave Ejezie <evergreendave1@gmail.com>
2022-11-17 10:22:30 +0000

    modify

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint
+//jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,31 +7,32 @@ const path = require('path')
 const router = express.Router();
 
 const port = 3000;
+ 
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 router.get('/', (req, res) => {
-  res.sendFile(__dirname + `/html/index.html`);
+  res.sendFile(path.join(__dirname + `/html/index.html`));
 });
 
 router.get('/about', (req, res) => {
-  res.sendFile(__dirname + `/html/about.html`);
+  res.sendFile(path.join(__dirname + `/html/about.html`));
 });
 
 router.get('/mail', (req, res) => {
-  res.sendFile(__dirname + `/html/mail.html`);
+  res.sendFile(path.join(__dirname + `/html/mail.html`));
 });
 
 router.get('/project', (req, res) => {
-  res.sendFile(__dirname + `/html/project.html`);
+  res.sendFile(path.join(__dirname + `/html/project.html`));
 });
 
 app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-//process.env.port
+//process.env.PORT || 
